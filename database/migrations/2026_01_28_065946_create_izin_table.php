fda09ce776431364f570e53e6b5024ff3bcd270e
zazqiaal18-arch <zazqia.al18@smk.belajar.id>
2026-01-30 09:19:59 +0700

    model sama migrate

--- database/migrations/2026_01_28_065946_create_izin_table.php
+++ database/migrations/2026_01_28_065946_create_izin_table.php
@@ -14,7 +14,13 @@ class CreateIzinTable extends Migration
     public function up()
     {
         Schema::create('izin', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('izin_id');
+            $table->string('nip'); 
+            $table->enum('jenis_izin', ['sakit', 'izin', 'kehamilan']);
+            $table->date('tanggal_mulai');
+            $table->date('tanggal_selesai');
+            $table->string('bukti_dokumen')->nullable();
+            $table->text('keterangan')->nullable();
             $table->timestamps();
         });
     }
