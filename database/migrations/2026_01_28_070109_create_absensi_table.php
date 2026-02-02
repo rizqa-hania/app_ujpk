458d184377ae121dddb70182c7dc768ba0d5ac15
astirianti <asti.rianti113@smk.belajar.id>
2026-02-02 10:02:46 +0700

    iyuppp

--- database/migrations/2026_01_28_070109_create_absensi_table.php
+++ database/migrations/2026_01_28_070109_create_absensi_table.php
@@ -15,31 +15,18 @@ class CreateAbsensiTable extends Migration
     {
         Schema::create('absensi', function (Blueprint $table) {
             $table->bigIncrements('absensi_id');
-            $table->foreignId('nip')->references('nip')->on('karyawan');
+            $table->foreignId('nip')->references('nip')->on('karyawan')->unique();
             $table->foreignId('kantor_id')->references('kantor_id') ->on('kantor');
-            $table->date('tanggal');
+            $table->date('tanggal')->unique();
             $table->time('jam_masuk')->nullable();
             $table->time('jam_pulang')->nullable();
             $table->decimal('lat_masuk', 10, 7)->nullable();
             $table->decimal('long_masuk', 10, 7)->nullable();
             $table->float('jarak_masuk')->nullable();
-            $table->enum('status', [
-            'hadir',
-            'izin',
-            'sakit',
-            'hamil',
-            'alpha',
-            'lembur'
-        ]);
-
+            $table->enum('status', ['hadir', 'izin', 'sakit', 'hamil', 'alpha', 'lembur' ]);
             $table->timestamps();
-<<<<<<< HEAD:database/migrations/2026_01_28_041702_create_absensi_table.php
-             $table->unique(['nip', 'tanggal']);
-=======
-
-            $table->unique(['nip', 'tanggal']);
->>>>>>> 9a6b93d8130c25ca1e42f8ede4404508cc6b8e34:database/migrations/2026_01_28_070109_create_absensi_table.php
         });
+          
     }
 
     /**
