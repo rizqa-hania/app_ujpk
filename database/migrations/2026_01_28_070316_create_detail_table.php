529b611ea160e7cc0682e3d78a67155011251ac2
rizqa-hania <rizqa.hania39@smk.belajar.id>
2026-02-11 15:19:03 +0700

    detail tabel

--- database/migrations/2026_01_28_070316_create_detail_table.php
+++ database/migrations/2026_01_28_070316_create_detail_table.php
@@ -16,10 +16,11 @@ class CreateDetailTable extends Migration
         Schema::create('detail', function (Blueprint $table) {
             $table->bigIncrements('detail_id');
             $table->foreignId('penggajian_id')->references('penggajian_id')->on('penggajian');
-            // izin yaa riss, gomenn ku ganti
             $table->string('nip', 30);
             $table->foreign('nip')->references('nip')->on('karyawan')->onUpdate('cascade')->onDelete('cascade');
-            $table->decimal('total_penghasilan', 15, 2);
+            $table->string('kode');
+            $table->foreign('kode')->references('kode')->on('komponen');
+            $table->decimal('total_pendapatan', 15, 2);
             $table->decimal('total_potongan', 15, 2);
             $table->decimal('gaji_bersih', 15, 2);
             $table->timestamps();
