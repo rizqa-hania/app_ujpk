ecd67d57217d1c30186490246b683807bc549cb6
rizqa-hania <rizqa.hania39@smk.belajar.id>
2026-02-26 13:49:38 +0700

    detail

--- resources/views/komponen/index.blade.php
+++ resources/views/komponen/index.blade.php
@@ -1,6 +1,5 @@
 @extends('template.layout')
 @section('content')
-<<<<<<< HEAD
 <div class="row">
     <div class="col-12">
         <div class="card">
@@ -63,69 +62,6 @@
                                             <button type="submit" class="btn btn-success btn-sm" onclick="return confirm('Nonaktifkan komponen ini?')">Nonaktifkan</button>
                                         </form>
                                     @endif
-=======
-<div class="card">
-    <div class="card-header">
-        <h3 class="card-title">Data Komponen Gaji</h3>
-        <div class="card-tools">
-            <a href="{{ route('komponen.create') }}" class="btn btn-primary btn-sm">
-                <i class="fas fa-plus"></i> Komponen
-            </a>
-        </div>
-    </div>
-    <div class="card-body table-responsive">
-        <table id="table" class="table table-stripped table-hover">
-            <thead>
-                <tr>
-                    <th>No</th>
-                    <th>Kode</th>
-                    <th>Komponen</th>
-                    <th>Tipe Komponen</th>
-                    <th>Tipe Penghitungan</th>
-                    <th>Nilai</th>
-                    <th>Status</th>
-                    <th>Aksi</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($komponen as $v)
-                <tr>
-                    <td>{{ $loop->iteration }}</td>
-                    <td>{{ $v->kode }}</td>
-                    <td>{{ $v->komponen }}</td>
-                    <td>{{ $v->tipe }}</td>
-                    <td>{{ $v->tipe_penghitungan == 'presentase' ? 'Persentase' : 'Tetap' }}</td>
-                    <td>
-                        @if($v->tipe_penghitungan == 'presentase')
-                            {{ $v->nilai }}%
-                        @else
-                            Rp {{ number_format($v->nilai, 0, ',', '.') }}
-                        @endif
-                    </td>
-                    <td>
-                        @if ($v->status == 0)
-                        <span class="badge badge-secondary">-</span>
-                        @elseif($v->status == 1)
-                            <span class="badge badge-success">Aktif</span>
-                        @else
-                            <span class="badge badge-danger">Nonaktif</span>
-                        @endif
-                    </td>
-                    <td class="text-center">
-                        <div class="d-flex justify-content-center">
-                            @if($v->status == 0 || $v->status == 2) {{-- Jika Nonaktif --}}
-                                <form action="{{ route('komponen.aktif', $v->kode) }}" method="POST" class="mr-1">
-                                    @csrf @method('PUT')
-                                    <button type="submit" class="btn btn-info btn-sm" onclick="return confirm('Aktifkan komponen ini?')">Aktifkan</button>
-                                </form>
-                            @else {{-- Jika Aktif --}}
-                                <form action="{{ route('komponen.nonaktif', $v->kode) }}" method="POST" class="mr-1">
-                                    @csrf @method('PUT')
-                                    <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Nonaktifkan komponen ini?')">Nonaktifkan</button>
-                                </form>
-                            @endif
->>>>>>> 33b1abb40bc3a6c1d249b63d1b934b65b1824e5c
-
                                     <form action="{{ route('komponen.destroy', $v->kode) }}" method="POST">
                                         @csrf @method('DELETE')
                                         <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Yakin hapus komponen ini?')">Hapus</button>
